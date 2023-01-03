019358d44a7d2f66747c8124088d649ce1659972
Agustín Durán <agustineduran@gmail.com>
2023-01-03 06:37:58 -0300

    Feat(user): call customized exception

--- src/Example/User/Application/Find/UserFindAllUseCase.php
+++ src/Example/User/Application/Find/UserFindAllUseCase.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Src\Example\User\Application\Find;
 
+use Src\Example\User\Domain\Exceptions\UserException;
+
 final class UserFindAllUseCase
 {
     public function __construct()
@@ -13,6 +15,7 @@ final class UserFindAllUseCase
 
     public function __invoke(): array
     {
+        throw new UserException('User not found', 404);
         return [
             "gretting" => "hello world on use case"
         ];
