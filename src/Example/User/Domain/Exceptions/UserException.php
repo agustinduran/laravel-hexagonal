8d4214f56c1bcc83a8f614246dbc6ed88c0c66a2
Agustín Durán <agustineduran@gmail.com>
2023-01-03 06:37:35 -0300

    Feat(core): create test customized exception

---
+++ src/Example/User/Domain/Exceptions/UserException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Example\User\Domain\Exceptions;
+
+use Src\Example\Shared\Domain\Exceptions\CustomException;
+
+final class UserException extends CustomException
+{
+    
+}
