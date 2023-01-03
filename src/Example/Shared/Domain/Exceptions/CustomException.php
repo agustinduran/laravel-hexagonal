a760946d9c5049ec2bd63b34e5be04fa6f68cc5b
Agustín Durán <agustineduran@gmail.com>
2023-01-03 06:36:52 -0300

    Feat(core): custom exception uncoupled

---
+++ src/Example/Shared/Domain/Exceptions/CustomException.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Src\Example\Shared\Domain\Exceptions;
+
+class CustomException extends \Exception
+{
+    // Return a default json when has a exception
+    public function toException(): array
+    {
+        $classTemp = new \ReflectionClass(get_class($this));
+        $class = explode('\\', $classTemp->getName());
+        return [
+            'status'  => $this->getCode(),
+            'error '  => true,
+            'class'   => end($class),
+            'message' => $this->getMessage()
+        ];
+    }
+}
