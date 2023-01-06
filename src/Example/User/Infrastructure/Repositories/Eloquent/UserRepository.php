85e84e2a01b569e2c1b453126983bc4f6d14228d
Agustín Durán <agustineduran@gmail.com>
2023-01-06 15:03:20 -0300

    Feat(user): add new method to contract

--- src/Example/User/Infrastructure/Repositories/Eloquent/UserRepository.php
+++ src/Example/User/Infrastructure/Repositories/Eloquent/UserRepository.php
@@ -6,6 +6,7 @@ namespace Src\Example\User\Infrastructure\Repositories\Eloquent;
 
 use Src\Example\User\Infrastructure\Repositories\Eloquent\User;
 use Src\Example\User\Domain\Contracts\UserRepositoryContract;
+use Src\Example\User\Domain\ValueObjects\UserId;
 
 final class UserRepository implements UserRepositoryContract
 {
@@ -18,5 +19,10 @@ final class UserRepository implements UserRepositoryContract
     public function findAll(): array
     {
         return $this->model->all()->toArray();
+    }
+
+    public function findById(UserId $id): ?array
+    {
+        return $this->model->find($id->getValue())?->toarray();
     }
 }
