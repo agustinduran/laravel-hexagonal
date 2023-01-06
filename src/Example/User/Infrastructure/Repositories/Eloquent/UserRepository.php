df24edc914e7c4e0f3a0dad0f48a63000ebd83ff
Agustín Durán <agustineduran@gmail.com>
2023-01-06 01:10:31 -0300

    Feat(user): get all users on repository

--- src/Example/User/Infrastructure/Repositories/Eloquent/UserRepository.php
+++ src/Example/User/Infrastructure/Repositories/Eloquent/UserRepository.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace Src\Example\User\Infrastructure\Repositories\Eloquent;
 
+use Src\Example\User\Infrastructure\Repositories\Eloquent\User;
 use Src\Example\User\Domain\Contracts\UserRepositoryContract;
 
 final class UserRepository implements UserRepositoryContract
 {
+    public function __construct(
+        private User $model
+    ) {
+        $model = new User;
+    }
+
     public function findAll(): array
     {
-        return ['hola mundo'];
+        return $this->model->all()->toArray();
     }
 }
