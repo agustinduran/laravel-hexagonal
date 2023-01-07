96337a4a57e642217c12619318572721cb47e21a
Agustín Durán <agustineduran@gmail.com>
2023-01-07 02:30:15 -0300

    Feat(user): add to inversion dependencies

--- src/Example/User/Infrastructure/Services/DependencyServiceProvider.php
+++ src/Example/User/Infrastructure/Services/DependencyServiceProvider.php
@@ -24,6 +24,11 @@ final class DependencyServiceProvider extends ServiceProvider
             ->when(\Src\Example\User\Application\Find\UserFindByIdUseCase::class)
             ->needs(\Src\Example\User\Domain\Contracts\UserRepositoryContract::class)
             ->give(\Src\Example\User\Infrastructure\Repositories\Eloquent\UserRepository::class);
+
+        $this->app
+            ->when(\Src\Example\User\Application\Delete\UserDeleteByIdUseCase::class)
+            ->needs(\Src\Example\User\Domain\Contracts\UserRepositoryContract::class)
+            ->give(\Src\Example\User\Infrastructure\Repositories\Eloquent\UserRepository::class);
     }
 
     /**
