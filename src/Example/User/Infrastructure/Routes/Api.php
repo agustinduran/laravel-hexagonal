3ef4b3b666b4709a13802a225391c2ed64f23e33
Agustín Durán <agustineduran@gmail.com>
2023-01-07 02:30:53 -0300

    Feat(user): create route for delete

--- src/Example/User/Infrastructure/Routes/Api.php
+++ src/Example/User/Infrastructure/Routes/Api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', UserFindAllController::class);
 Route::get('/{id}', UserFindByIdController::class);
+Route::delete('/{id}', UserDeleteByIdController::class);
