05521b36addac9094d3b6cfa65a9fbbdf4087bc8
Agustín Durán <agustineduran@gmail.com>
2023-01-09 10:38:35 -0300

    Feat(user): abstract method create

--- src/Example/User/Domain/Contracts/UserRepositoryContract.php
+++ src/Example/User/Domain/Contracts/UserRepositoryContract.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Src\Example\User\Domain\Contracts;
 
+use Src\Example\User\Domain\ValueObjects\UserCreateRequest;
 use Src\Example\User\Domain\ValueObjects\UserId;
 
 interface UserRepositoryContract
 {
     public function findAll(): array;
     public function findById(UserId $id): ?array;
+    public function create(UserCreateRequest $request): ?array;
     public function deleteById(UserId $id): bool;
 }
--- src/Example/User/Infrastructure/Repositories/Eloquent/UserRepository.php
+++ src/Example/User/Infrastructure/Repositories/Eloquent/UserRepository.php
@@ -6,6 +6,7 @@ namespace Src\Example\User\Infrastructure\Repositories\Eloquent;
 
 use Src\Example\User\Infrastructure\Repositories\Eloquent\User;
 use Src\Example\User\Domain\Contracts\UserRepositoryContract;
+use Src\Example\User\Domain\ValueObjects\UserCreateRequest;
 use Src\Example\User\Domain\ValueObjects\UserId;
 
 final class UserRepository implements UserRepositoryContract
@@ -24,6 +25,11 @@ final class UserRepository implements UserRepositoryContract
     public function findById(UserId $id): ?array
     {
         return $this->model->where('id', $id->getValue())->whereNull('deleted_at')->get()->toArray();
+    }
+
+    public function create(UserCreateRequest $request): ?array
+    {
+        return $this->model->create($request->getValue())->toArray();
     }
 
     public function deleteById(UserId $id): bool
