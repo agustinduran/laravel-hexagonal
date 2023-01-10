d8544ce3bb1ad2aa502442875271c4ea1952cf0b
Agustín Durán <agustineduran@gmail.com>
2023-01-09 23:52:27 -0300

    Feat(user): add parameter data to exception

--- src/Example/Shared/Domain/Exceptions/CustomException.php
+++ src/Example/Shared/Domain/Exceptions/CustomException.php
@@ -3,18 +3,31 @@ declare(strict_types=1);
 
 namespace Src\Example\Shared\Domain\Exceptions;
 
+use Throwable;
+
 class CustomException extends \Exception
 {
+    public function __construct(
+        string $message = "",
+        int $code = 0,
+        ?Throwable $previous = null,
+        private ?array $data
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+
     // Return a default json when has a exception
     public function toException(): array
     {
         $classTemp = new \ReflectionClass(get_class($this));
         $class = explode('\\', $classTemp->getName());
-        return [
+        $response = [
             'status'  => $this->getCode(),
             'success' => false,
             'class'   => end($class),
-            'message' => $this->getMessage()
+            'message' => $this->getMessage(),
         ];
+        if ($this->data) $response['data'] = $this->data;
+        return $response;
     }
 }
